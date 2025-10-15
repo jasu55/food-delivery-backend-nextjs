333aa197120dd2f74f20d9e5ac5498a2d12f4c93
jasu55 <jasu14707@gmail.com>
2025-10-15 11:09:24 +0800

    ee

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <p className="animate-wiggle text-6xl font-extrabold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-        Hello Back end
+        Hello Back end xxaa
       </p>
     </div>
   );
